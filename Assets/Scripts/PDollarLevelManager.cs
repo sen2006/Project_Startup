18140c61785c1f8c47a3921bd3f9d498774983ce
Bog558 <bodeabratish@gmail.com>
2025-01-29 19:18:42 +0100

    Lamp added

--- Assets/Scripts/PDollarLevelManager.cs
+++ Assets/Scripts/PDollarLevelManager.cs
@@ -11,11 +11,16 @@ public class PDollarLevelManager : MonoBehaviour
     string correctAnswer;
     [SerializeField] string currentAnswer;
     MovementRecognizer movementRecognizer;
+    [SerializeField] TMPro.TextMeshPro givenAnswerDisplay;
+    [SerializeField] TMPro.TextMeshPro questionDisplay;
+    int currentQuestionID = 0;
     void Start()
     {
+        givenAnswerDisplay.text = "";
         movementRecognizer = GetComponent<MovementRecognizer>();
         movementRecognizer.gestureOutput += InputString;
         currentQuestion = questions.Keys.First();
+        questionDisplay.text = "What is the translation for:\n" + currentQuestion;
     }
 
     public void InputString(string input)
@@ -24,20 +29,25 @@ public class PDollarLevelManager : MonoBehaviour
         if (input == "back")
         {
             if (currentAnswer.Length > 0)
-                currentAnswer = currentAnswer.Remove(currentAnswer.Last());
+                currentAnswer = currentAnswer.Remove(currentAnswer.Length-1);
         }
         else if (input == "send")
         {
-            // TODO input code for confirming the answer
-            if (questions[currentQuestion] == currentAnswer)
+            Debug.Log("comparing: "+ questions[currentQuestion].ToLower() +" to "+ currentAnswer.ToLower());
+            if (questions[currentQuestion].ToLower() == currentAnswer.ToLower())
             {
                 Debug.Log("the correct answer was given");
+                currentQuestion = questions.Keys[++currentQuestionID];
+                questionDisplay.text = "What is the translation for:\n" + currentQuestion;
+                currentAnswer = "";
             } else
             {
                 Debug.Log("the wrong answer was given");
+                currentAnswer = "Try again";
             }
         }
         else if (input.Length>1) return;
-        currentAnswer += input;
+        else currentAnswer += input;
+        givenAnswerDisplay.text = currentAnswer;
     }
 }
